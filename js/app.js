d007a4777b1658d5697a0fc44c7e19c6f38993cd
oscar machicado <machicado.macias@gmail.com>
2022-11-09 22:24:38 -0300

    originales

---
+++ js/app.js
@@ -0,0 +1,44 @@
+var nombre = 'oscar';
+console.log(nombre);
+
+var fisrtNumber = 20;
+var secondNumber = 5;
+
+
+
+//Suma
+var plus = fisrtNumber + secondNumber;
+console.log('Operador Suma +');
+console.log(plus);
+//Resta
+var subtraction = fisrtNumber - secondNumber;
+console.log('Operador Resta -');
+console.log(subtraction);
+//Multiplicación
+var multiplication = fisrtNumber * secondNumber;
+console.log('Operador Multiplicación *');
+console.log(multiplication);
+//División
+var division = fisrtNumber / secondNumber;
+console.log('Operador División /');
+console.log(division);
+//Módulo
+var module = fisrtNumber % secondNumber;
+console.log('Operador Módulo %');
+console.log(module);
+//primero
+document.querySelector('.primero').innerHTML = 'Bienvenido Pedro';
+//segundo
+let segundo = document.querySelector('.segundo');
+segundo.style.backgroundColor = 'pink';
+//tercero
+let tercero = document.querySelector('.tercero');
+tercero.style.backgroundColor = 'gray';
+tercero.style.color = 'White';
+segundo.style.backgroundColor = 'black'
+
+let footeress = document.querySelector('.footeress');
+footeress.style.backgroundColor = 'gray';
+
+
+
